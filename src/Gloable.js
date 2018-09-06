b4ac60a793bacc5b6d5451007d9ea3d5fdf206df
zedong <zedong@staff.weibo.com>
2018-09-06 11:46:06 +0800

    update

--- src/Gloable.js
+++ src/Gloable.js
@@ -48,3 +48,66 @@ export var BLOCK = {
 	moveDownVelocity: 0.07,
 	fullHeight: 5.5 / 21 * 40
 };
+export var PARTICLE = {
+	radius: 0.3,
+	detail: 2
+};
+
+export  var GAME  = {
+	BOTTOMBOUND: -55,
+	TOPBOUND: 41,
+	gravity: 720,
+	//gravity: 750,
+	touchmoveTolerance: 20,
+	LEFTBOUND: -140,
+	topTrackZ: -30,
+	rightBound: 90,
+	HEIGHT: window.innerHeight > window.innerWidth ? window.innerHeight : window.innerWidth,
+	WIDTH: window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth,
+	canShadow: true
+};
+
+export var WAVE = {
+	innerRadius: 2.2,
+	outerRadius: 3,
+	thetaSeg: 25
+};
+
+export var CAMERA = {
+	fov: 60
+};
+
+export var AUDIO  = {
+	success: 'res/success.mp3',
+	perfect: 'res/perfect.mp3',
+	scale_loop: 'res/scale_loop.mp3',
+	scale_intro: 'res/scale_intro.mp3',
+	restart: 'res/start.mp3',
+	fall: 'res/fall.mp3',
+	fall_2: 'res/fall_2.mp3',
+	combo1: 'res/combo1.mp3',
+	combo2: 'res/combo2.mp3',
+	combo3: 'res/combo3.mp3',
+	combo4: 'res/combo4.mp3',
+	combo5: 'res/combo5.mp3',
+	combo6: 'res/combo6.mp3',
+	combo7: 'res/combo7.mp3',
+	combo8: 'res/combo8.mp3',
+	icon: 'res/icon.mp3',
+	pop: 'res/pop.mp3',
+	sing: 'res/sing.mp3',
+	store: 'res/store.mp3',
+	water: 'res/water.mp3'
+};
+
+export var FRUSTUMSIZE  = window.innerHeight / window.innerWidth / 736 * 414 * 60;
+
+export var loader  = new THREE.TextureLoader();
+
+export var cylinder_shadow = new THREE.MeshBasicMaterial({ map: loader.load('res/cylinder_shadow.png'), transparent: true, alphaTest: 0.01 });
+export var desk_shadow  = new THREE.MeshBasicMaterial({ map: loader.load('res/desk_shadow.png'), transparent: true, alphaTest: 0.01 });
+export var shadow = new THREE.MeshBasicMaterial({ map: loader.load('res/shadow.png'), transparent: true, alphaTest: 0.01 });
+export var grayMaterial  = new THREE.MeshLambertMaterial({ map: loader.load('res/gray.png') });
+export var numberMaterial = new THREE.MeshLambertMaterial({ map: loader.load('res/number.png'), alphaTest: 0.6 });
+
+export var REPORTERTIMEOUT  = 60001;
